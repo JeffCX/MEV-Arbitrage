98096959ea17ec17004a971331b55e274eb9b053
JeffCX <xc1008@nyu.edu>
2022-05-13 12:17:56 -0400

    Update Query.sol

--- contract/Query.sol
+++ contract/Query.sol
@@ -1,81 +1,44 @@
 //SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.6.12;
+pragma solidity 0.8.7;
 
 pragma experimental ABIEncoderV2;
 
-interface IERC20 {
-    event Approval(address indexed owner, address indexed spender, uint value);
-    event Transfer(address indexed from, address indexed to, uint value);
-
-    function name() external view returns (string memory);
-    function symbol() external view returns (string memory);
-    function decimals() external view returns (uint8);
-    function totalSupply() external view returns (uint);
-    function balanceOf(address owner) external view returns (uint);
-    function allowance(address owner, address spender) external view returns (uint);
-
-    function approve(address spender, uint value) external returns (bool);
-    function transfer(address to, uint value) external returns (bool);
-    function transferFrom(address from, address to, uint value) external returns (bool);
+interface IUniswapV2Pair {
+    function token0() external view returns (address);
+    function token1() external view returns (address);
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
 }
 
-interface IWETH is IERC20 {
-    function deposit() external payable;
-    function withdraw(uint) external;
+abstract contract UniswapV2Factory  {
+    mapping(address => mapping(address => address)) public getPair;
+    address[] public allPairs;
+    function allPairsLength() external view virtual returns (uint);
 }
 
-// This contract simply calls multiple targets sequentially, ensuring WETH balance before and after
-
-contract FlashBotsMultiCall {
-    address private immutable owner;
-    address private immutable executor;
-    IWETH private constant WETH = IWETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-
-    modifier onlyExecutor() {
-        require(msg.sender == executor);
-        _;
-    }
-
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
-
-    constructor(address _executor) public payable {
-        owner = msg.sender;
-        executor = _executor;
-        if (msg.value > 0) {
-            WETH.deposit{value: msg.value}();
+// In order to quickly load up data from Uniswap-like market, this contract allows easy iteration with a single eth_call
+contract BatchUniswapQuery {
+    function getReservesByPairs(IUniswapV2Pair[] calldata _pairs) external view returns (uint256[3][] memory) {
+        uint256[3][] memory result = new uint256[3][](_pairs.length);
+        for (uint i = 0; i < _pairs.length; i++) {
+            (result[i][0], result[i][1], result[i][2]) = _pairs[i].getReserves();
         }
+        return result;
     }
 
-    receive() external payable {
-    }
-
-    function uniswapWeth(uint256 _wethAmountToFirstMarket, uint256 _ethAmountToCoinbase, address[] memory _targets, bytes[] memory _payloads) external onlyExecutor payable {
-        require (_targets.length == _payloads.length);
-        uint256 _wethBalanceBefore = WETH.balanceOf(address(this));
-        WETH.transfer(_targets[0], _wethAmountToFirstMarket);
-        for (uint256 i = 0; i < _targets.length; i++) {
-            (bool _success, bytes memory _response) = _targets[i].call(_payloads[i]);
-            require(_success); _response;
+    function getPairsByIndexRange(UniswapV2Factory _uniswapFactory, uint256 _start, uint256 _stop) external view returns (address[3][] memory)  {
+        uint256 _allPairsLength = _uniswapFactory.allPairsLength();
+        if (_stop > _allPairsLength) {
+            _stop = _allPairsLength;
         }
-
-        uint256 _wethBalanceAfter = WETH.balanceOf(address(this));
-        require(_wethBalanceAfter > _wethBalanceBefore + _ethAmountToCoinbase);
-        if (_ethAmountToCoinbase == 0) return;
-
-        uint256 _ethBalance = address(this).balance;
-        if (_ethBalance < _ethAmountToCoinbase) {
-            WETH.withdraw(_ethAmountToCoinbase - _ethBalance);
+        require(_stop >= _start, "start cannot be higher than stop");
+        uint256 _qty = _stop - _start;
+        address[3][] memory result = new address[3][](_qty);
+        for (uint i = 0; i < _qty; i++) {
+            IUniswapV2Pair _uniswapPair = IUniswapV2Pair(_uniswapFactory.allPairs(_start + i));
+            result[i][0] = _uniswapPair.token0();
+            result[i][1] = _uniswapPair.token1();
+            result[i][2] = address(_uniswapPair);
         }
-        block.coinbase.transfer(_ethAmountToCoinbase);
-    }
-
-    function call(address payable _to, uint256 _value, bytes calldata _data) external onlyOwner payable returns (bytes memory) {
-        require(_to != address(0));
-        (bool _success, bytes memory _result) = _to.call{value: _value}(_data);
-        require(_success);
-        return _result;
+        return result;
     }
 }
